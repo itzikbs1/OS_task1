fb298370dff0fd44f1b648c97f5a1853f47aab02
Gal Cohen <92460450+Gal25@users.noreply.github.com>
2022-11-16 20:53:21 +0200

    Update codec.h

---
+++ codec.h
@@ -0,0 +1,12 @@
+#include "codec1.c"
+#include "codec2.c"
+
+
+///////////// ENCODE AND DECODE 1 ///////////////
+
+void encode(char * src, char * dst, int len); //Encoder converts uppercase chars to lowercase and vise versa.
+
+void decode(char * src, char * dst, int len); //decode to the original string
+
+
+///////////// ENCODE AND DECODE 2 ///////////////
